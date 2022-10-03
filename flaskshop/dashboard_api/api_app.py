f53e21d81b6050ec9b9a8538d19b7025d19cecf1
nazarKobryn <kobrinnazar05@gmail.com>
2022-10-03 16:06:20 +0300

    created new api endpoint for delete image

--- flaskshop/dashboard_api/api_app.py
+++ flaskshop/dashboard_api/api_app.py
@@ -18,6 +18,7 @@ from .views import (
     dashboard_menu_del,
     site_page_del,
     site_menu_del,
+    dashboard_product_delete_image,
 )
 
 
@@ -95,4 +96,10 @@ dashboard_api.add_url_rule(
 )
 dashboard_api.add_url_rule(
     "/site_menus/<int:id>/delete", view_func=site_menu_del, methods=["DELETE"]
+)
+
+dashboard_api.add_url_rule(
+    "/products/image/delete",
+    view_func=dashboard_product_delete_image,
+    methods=["DELETE"],
 )
